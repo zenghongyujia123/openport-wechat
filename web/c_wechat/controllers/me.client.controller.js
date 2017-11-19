0305d110286a82dae6efc2d39022a0d16422983a
zenghongyujia123 <1963968619@qq.com>
2017-11-19 15:59:09 +0800

    Clear code

--- web/c_wechat/controllers/me.client.controller.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  var vipTime = $('.vip-time').text();
-  if (vipTime) {
-    var start = new Date(vipTime);
-    var end = new Date(new Date(vipTime).setMonth(new Date(vipTime).getMonth() + 2));
-    var date = getDurationDays(start, end);
-    $('.vip-time').text('您的会员将在' + date + '后失效');
-  }
-});
