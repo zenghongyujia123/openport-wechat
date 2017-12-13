27e5aa181bcd9c4c1f57e3616537da13e571df13
zenghongyujia123 <1963968619@qq.com>
2017-12-14 01:17:37 +0800

    Update home page style

--- server/controllers/page_wechat.server.controller.js
+++ server/controllers/page_wechat.server.controller.js
@@ -39,10 +39,47 @@ exports.page_detail = function (req, res, next) {
   return res.render(filepath, {});
 };
 
+exports.page_expense_list = function (req, res, next) {
+  var filepath = path.join(__dirname, '../../web/c_wechat/views/page_expense_list.client.view.html');
+  return res.render(filepath, {});
+};
+
+exports.page_expense_detail = function (req, res, next) {
+  var filepath = path.join(__dirname, '../../web/c_wechat/views/page_expense_detail.client.view.html');
+  return res.render(filepath, {});
+};
+
+
 
 exports.page_signin = function (req, res, next) {
-
   var filepath = path.join(__dirname, '../../web/c_wechat/views/page_signin.client.view.html');
   return res.render(filepath, {});
 };
+
+
+exports.page_expense_add = function (req, res, next) {
+  var filepath = path.join(__dirname, '../../web/c_wechat/views/page_expense_add.client.view.html');
+  return res.render(filepath, {});
+};
+
+exports.page_profile = function (req, res, next) {
+  var filepath = path.join(__dirname, '../../web/c_wechat/views/page_profile.client.view.html');
+  return res.render(filepath, {});
+};
+
+exports.page_search = function (req, res, next) {
+  var filepath = path.join(__dirname, '../../web/c_wechat/views/page_search.client.view.html');
+  return res.render(filepath, {});
+};
+
+exports.page_setting = function (req, res, next) {
+  var filepath = path.join(__dirname, '../../web/c_wechat/views/page_setting.client.view.html');
+  return res.render(filepath, {});
+};
+
+// exports.page_signin = function (req, res, next) {
+//   var filepath = path.join(__dirname, '../../web/c_wechat/views/page_signin.client.view.html');
+//   return res.render(filepath, {});
+// };
+
 
--- server/routes/page_wechat.server.route.js
+++ server/routes/page_wechat.server.route.js
@@ -7,5 +7,12 @@ var index = require('../controllers/page_wechat');
 module.exports = function (app) {
   app.route('/page_wechat/page_home').get(index.page_home);
   app.route('/page_wechat/page_detail').get(index.page_detail);
+  app.route('/page_wechat/page_expense_list').get(index.page_expense_list);
+  app.route('/page_wechat/page_expense_detail').get(index.page_expense_detail);
   app.route('/page_wechat/page_signin').get(index.page_signin);
+
+  app.route('/page_wechat/page_expense_add').get(index.page_expense_add);
+  app.route('/page_wechat/page_profile').get(index.page_profile);
+  app.route('/page_wechat/page_search').get(index.page_search);
+  app.route('/page_wechat/page_setting').get(index.page_setting);
 };
