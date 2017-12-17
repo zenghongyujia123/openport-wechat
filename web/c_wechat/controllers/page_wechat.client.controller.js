5ee054bc0a4cd302b44669a9c6e626d2fdb5328b
zenghongyujia123 <1963968619@qq.com>
2017-12-17 21:36:09 +0800

    Update for test

--- web/c_wechat/controllers/page_wechat.client.controller.js
+++ web/c_wechat/controllers/page_wechat.client.controller.js
@@ -27,7 +27,7 @@ function chooseImage(callback) {
     sourceType: ['album'], // 可以指定来源是相册还是相机，默认二者都有
     success: function (res) {
       var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-      return callback(localIds.localIds);
+      return callback(localIds);
     }
   });
 }
@@ -39,7 +39,7 @@ function takeCamera(callback) {
     sourceType: ['camera'], // 可以指定来源是相册还是相机，默认二者都有
     success: function (res) {
       var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-      return callback(localIds.localIds);
+      return callback(localIds);
     }
   });
 }
