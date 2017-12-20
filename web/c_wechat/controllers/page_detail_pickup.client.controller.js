e607bac21145265071851fb469af0d2a6acabfea
zenghongyujia123 <1963968619@qq.com>
2017-12-21 02:01:34 +0800

    Update for test

--- web/c_wechat/controllers/page_detail_pickup.client.controller.js
+++ web/c_wechat/controllers/page_detail_pickup.client.controller.js
@@ -55,7 +55,7 @@ $(function () {
   });
 
   function clickReason(str) {
-    $('.pickupCountReasonCode').text(this.text);
+    $('.pickupCountReasonCode').val(this.text);
   }
 
   $('.pickupCountReasonCode').click(function () {
@@ -107,7 +107,7 @@ $(function () {
   $('.submit-pickup').click(function () {
     var pickedUpQty = $('.pickedUpQty').val();
     var pickupLoaderName = $('.pickupLoaderName').val();
-    var pickupCountReasonCode = $('.pickupCountReasonCode').text();
+    var pickupCountReasonCode = $('.pickupCountReasonCode').val();
     var cartonCount = $('.cartonCount').text();
     var id = this.id;
     if (!pickedUpQty) {
